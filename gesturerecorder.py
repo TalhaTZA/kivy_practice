d83da179204ecc6ae67f8657cd5fc28e28f97463
TalhaTZA <talhavjtza@gmail.com>
2017-11-19 23:56:03 +0500

    gesture with screen manager

--- gesturerecorder.py
+++ gesturerecorder.py
@@ -4,14 +4,19 @@ from kivy.uix.floatlayout import FloatLayout
 import kivy.uix , math
 from kivy.graphics import Line , Ellipse
 from kivy.gesture import Gesture , GestureDatabase
+from kivy.uix.screenmanager import ScreenManager ,Screen
+from kivy.graphics import Color
 
 
-class GestureRecorder(FloatLayout):
+class GestureRecorder(Screen):
+    
     def on_touch_down(self,touch):
         self.points = [touch.pos]
+        color_picker=self.color_picker
         with self.canvas:
+            Color(*color_picker.color)
             Ellipse(pos=(touch.x-5,touch.y-5),size=(10,10))
-            self.Line=Line(points=(touch.x,touch.y))
+            self.Line=Line(points=(touch.x,touch.y),width=1.5)
     
     def on_touch_move(self,touch):
         self.points+=[touch.pos]
@@ -25,13 +30,16 @@ class GestureRecorder(FloatLayout):
         gesture.add_stroke(self.points)
         gesture.normalize()
         gdb=GestureDatabase()
-        print ("Gesture:",gdb.gesture_to_str(gesture).decode(encoding='UTF-8',))
-        for ele in self.points:
-            print(ele)
+        #print ("Gesture:",gdb.gesture_to_str(gesture).decode(encoding='UTF-8',))
+        #for ele in self.points:
+            #print(ele)
+
+class MultipleScreens(ScreenManager):
+    pass
 
 class GestureRecorderApp(App):
     def build(self):
-        return GestureRecorder()
+        return MultipleScreens()
 
 if __name__ in ("__main__"):
     GestureRecorderApp().run()
